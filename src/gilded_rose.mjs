2efa604e99a78da79f38c600746775a4fc356d26
王昱 <wangyu2021@foxmail.com>
2025-05-08 13:51:31 +0800

    change the code and pass the tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -30,27 +30,26 @@ export class Shop {
           }
           this.items[i].quality = Math.min(this.items[i].quality, 50);
         }
-
         break;
       } else if (this.items[i].name === "Aged Brie") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-        }
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].quality < 50) {
+          if (this.items[i].sellIn < 0) {
             this.items[i].quality = this.items[i].quality + 1;
           }
+          this.items[i].quality = Math.min(this.items[i].quality, 50);
+          break;
         }
       } else {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
         if (this.items[i].quality > 0) {
           this.items[i].quality = this.items[i].quality - 1;
-        }
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].quality > 0) {
+          if (this.items[i].sellIn < 0) {
             this.items[i].quality = this.items[i].quality - 1;
           }
+          this.items[i].quality = Math.max(this.items[i].quality, 0);
+          break
         }
       }
     }
