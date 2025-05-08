d452d36544b941b73cefddbdde14ffef3642d059
王昱 <wangyu2021@foxmail.com>
2025-05-08 13:28:44 +0800

    change the code and pass the tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -34,7 +34,17 @@ export class Shop {
         if (this.items[i].sellIn < 0) {
           this.items[i].quality = this.items[i].quality - this.items[i].quality;
         }
-      }else {
+      }else if(this.items[i].name === "Aged Brie") {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1;
+        }
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn < 0) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
+            }
+        }
+      } else {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].quality > 0) {
             this.items[i].quality = this.items[i].quality - 1;
