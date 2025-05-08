0bda5735d495db74b1610e9cb6ea8ccc62fe2a41
王昱 <wangyu2021@foxmail.com>
2025-05-08 11:29:22 +0800

    finish 1 test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -7,5 +7,15 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
+  });
+
+  test("Backstage passes to a TAFKAL80ETC concert 1", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(9);
+    expect(items[0].quality).to.equal(22);
   });
 });
