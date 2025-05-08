301243058d92efcca4294004368390cf895b509b
王昱 <wangyu2021@foxmail.com>
2025-05-08 13:31:03 +0800

    change the code and pass the tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -17,16 +17,14 @@ export class Shop {
       } else if(this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+          if (this.items[i].sellIn < 11) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+          }
+          if (this.items[i].sellIn < 6) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
             }
           }
         }
