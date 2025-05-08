646a019d73c137fbcd9ee16591f2066b111a3c74
王昱 <wangyu2021@foxmail.com>
2025-05-08 13:55:35 +0800

    change the code and pass the tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -1,3 +1,5 @@
+import { it } from "vitest";
+
 export class Item {
   constructor(name, sellIn, quality) {
     this.name = name;
@@ -12,45 +14,46 @@ export class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
-        break;
-      } else if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].sellIn < 0) {
-          this.items[i].quality = 0;
+      switch (this.items[i].name) {
+        case "Sulfuras, Hand of Ragnaros":
           break;
-        }
-        if (this.items[i].quality < 50) {
-          if (this.items[i].sellIn < 5) {
-            this.items[i].quality = this.items[i].quality + 3;
-          } else if (this.items[i].sellIn < 10) {
-            this.items[i].quality = this.items[i].quality + 2;
-          } else {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-          this.items[i].quality = Math.min(this.items[i].quality, 50);
-        }
-        break;
-      } else if (this.items[i].name === "Aged Brie") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          this.items[i].sellIn = this.items[i].sellIn - 1;
           if (this.items[i].sellIn < 0) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality = 0;
+            break;
           }
-          this.items[i].quality = Math.min(this.items[i].quality, 50);
+          if (this.items[i].quality < 50) {
+            if (this.items[i].sellIn < 5) {
+              this.items[i].quality = this.items[i].quality + 3;
+            } else if (this.items[i].sellIn < 10) {
+              this.items[i].quality = this.items[i].quality + 2;
+            } else {
+              this.items[i].quality = this.items[i].quality + 1;
+            }
+            this.items[i].quality = Math.min(this.items[i].quality, 50);
+          }
           break;
-        }
-      } else {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].quality > 0) {
-          this.items[i].quality = this.items[i].quality - 1;
-          if (this.items[i].sellIn < 0) {
+        case "Aged Brie":
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+            if (this.items[i].sellIn < 0) {
+              this.items[i].quality = this.items[i].quality + 1;
+            }
+            this.items[i].quality = Math.min(this.items[i].quality, 50);
+          }
+          break;
+        default:
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          if (this.items[i].quality > 0) {
             this.items[i].quality = this.items[i].quality - 1;
+            if (this.items[i].sellIn < 0) {
+              this.items[i].quality = this.items[i].quality - 1;
+            }
+            this.items[i].quality = Math.max(this.items[i].quality, 0);
           }
-          this.items[i].quality = Math.max(this.items[i].quality, 0);
-          break
-        }
+          break;
       }
     }
 
