89d21c48bdad8f3b1eac13d0d912f92fcf24f54c
王昱 <wangyu2021@foxmail.com>
2025-05-08 12:33:09 +0800

    add test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -165,6 +165,14 @@ describe("Gilded Rose - Mutation", () => {
     expect(items[0].quality).to.equal(50);
   });
 
+  test("Backstage passes to a TAFKAL80ETC concert 6", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 6, 49)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(5);
+    expect(items[0].quality).to.equal(50);
+  });
+
   test("foo 1", () => {
     const gildedRose = new Shop([new Item("foo", 50, 0)]);
     const items = gildedRose.updateQuality();
