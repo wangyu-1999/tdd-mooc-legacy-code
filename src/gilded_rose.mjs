040628d7dd5a02caa54f789d0722690ab2022969
王昱 <wangyu2021@foxmail.com>
2025-05-08 13:25:42 +0800

    change the code and pass the tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -14,6 +14,26 @@ export class Shop {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
         break
+      } else if(this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1;
+          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (this.items[i].sellIn < 11) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1;
+              }
+            }
+            if (this.items[i].sellIn < 6) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1;
+              }
+            }
+          }
+        }
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        }
       }else {
         if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
           if (this.items[i].quality > 0) {
