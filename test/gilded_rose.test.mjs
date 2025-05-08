42035434c464740e5b20e9f474760bd11351c67a
王昱 <wangyu2021@foxmail.com>
2025-05-08 11:36:00 +0800

    6 test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -50,4 +50,12 @@ describe("Gilded Rose", () => {
     expect(items[0].sellIn).to.equal(1);
     expect(items[0].quality).to.equal(23);
   });
+
+  test("Backstage passes to a TAFKAL80ETC concert 6", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 2, 90)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(1);
+    expect(items[0].quality).to.equal(90);
+  });
 });
