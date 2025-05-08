eaf98bfff7cffa552072b9fedabf4ce92db3c392
王昱 <wangyu2021@foxmail.com>
2025-05-08 13:41:41 +0800

    change the code and pass the tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -16,25 +16,25 @@ export class Shop {
         break
       } else if(this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].quality < 50) {
+        if (this.items[i].quality === 49) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].quality < 50) {
-            if (this.items[i].sellIn < 10) {
-              this.items[i].quality = this.items[i].quality + 1;
-            }
-            if (this.items[i].sellIn < 5) {
-              this.items[i].quality = this.items[i].quality + 1;
-            }
+        } else if (this.items[i].quality < 49) {
+          this.items[i].quality = this.items[i].quality + 1;
+          if (this.items[i].sellIn < 10) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+          if (this.items[i].sellIn < 5) {
+            this.items[i].quality = this.items[i].quality + 1;
           }
         }
         if (this.items[i].sellIn < 0) {
           this.items[i].quality = 0;
         }
       }else if(this.items[i].name === "Aged Brie") {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
         }
-        this.items[i].sellIn = this.items[i].sellIn - 1;
         if (this.items[i].sellIn < 0) {
             if (this.items[i].quality < 50) {
               this.items[i].quality = this.items[i].quality + 1;
